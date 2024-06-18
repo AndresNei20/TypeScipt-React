e170537d8e655a93d2ec2f24d84f636e2503e4c5
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-06-17 23:12:30 -0500

    react + TS and CourseGoal Componentent created

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { CourseGoal } from "./components/CourseGoal/CourseGoal";
+
+function App() {
+  return (
+    <>
+      <h1>Let's get started</h1>
+      <CourseGoal
+        title="hello"
+        description="hello i'm the description"
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ react-ts-basics/src/components/CourseGoal/CourseGoal.tsx
@@ -0,0 +1,17 @@
+export const CourseGoal = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+};
