806167f8cdf8214944ae6dde3606556838b62c6c
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-06-18 22:21:50 -0500

    FC prop type way

--- react-ts-basics/src/components/CourseGoal/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal/CourseGoal.tsx
@@ -1,4 +1,4 @@
-import {  ReactNode } from "react";
+import { type FC, ReactNode } from "react";
 /* import { PropsWithChildren } from "react"; */
 
 interface CourseGoalProps {
@@ -8,6 +8,19 @@ interface CourseGoalProps {
 
 /* type CourseGoalProps = PropsWithChildren<{ title: string } > */ 
 // * Theres other way to use an component that waits a children
+
+/* const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
+export default CourseGoal */ //This is OTHER way to use Prop types
 
 export const CourseGoal = ({
   title,
